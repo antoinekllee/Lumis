dbff39463380d08ea909a99b90c44c849e767cbd
Antoine-Lee <antoinekllee@gmail.com>
2024-12-29 18:31:18 +0800

    add world assets

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb = null; 
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+}
