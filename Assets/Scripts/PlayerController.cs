39d5d14e1a5d8045a59a166389a623f404c3f47d
AlexLee09 <alexandrekmlee@gmail.com>
2024-12-30 16:57:41 +0800

    Basic Character Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,55 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody rb = null; 
+    // Movement speed
+    [Header("Movement")]
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpForce = 5f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Flags
+    private bool isGrounded = true;
+    
+    // Components
+    private Rigidbody rb = null;
+
+    // Start is called before the first frame update
     private void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     private void Update()
     {
-        
+        // Movement input
+        float moveX = Input.GetAxis("Horizontal"); // A/D or Left/Right Arrow
+        float moveZ = Input.GetAxis("Vertical");   // W/S or Up/Down Arrow
+
+        // Calculate movement direction
+        Vector3 movement = new Vector3(moveX, 0, moveZ) * moveSpeed;
+
+        // Apply movement (change velocity on x/z axis)
+        Vector3 velocity = rb.linearVelocity;
+        velocity.x = movement.x;
+        velocity.z = movement.z;
+        rb.linearVelocity = velocity;
+
+        // Jump input
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    // Detect when the player touches the ground
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
     }
 }
